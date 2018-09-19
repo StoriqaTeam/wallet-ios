9ef46e68a538471b429593bfc07cba006a6ad01f
Daniil Miroshnichecko <daniilel1209@gmail.com>
2018-09-19 19:19:56 +0300

    Constants in swift style

--- Wallet/Common/Helpers/Constants.swift
+++ Wallet/Common/Helpers/Constants.swift
@@ -13,28 +13,28 @@ import Slog
 let log = Slog(level: .debug, useEmoji: true)
 
 
-class Constants {
+struct Constants {
     typealias Sizes = SizeConstants
     
-    class Keys {
+    struct Keys {
         static let kIsFirstLaunch = "isFirstLaunch"
         static let kIsQuickLaunchSet = "isQuickLaunchSet"
     }
     
-    class Errors {
+    struct Errors {
         static let serverResponse = "server_response_error".localized()
         static let unknown = "unknown_error".localized()
         static let userFriendly = "user_friendly_error".localized()
     }
     
-    class NetworkAuth {
+    struct NetworkAuth {
         static let kFacebookAppId = "fb425217154570731"
         static let kGoogleClientId = "245895770851-qlolkejdjnske28jmbdgs89969o0a7ec.apps.googleusercontent.com"
     }
 }
 
 
-class SizeConstants {
+struct SizeConstants {
     static let screenWith = UIScreen.main.bounds.size.width
     static let screenHeight = UIScreen.main.bounds.size.height
     
