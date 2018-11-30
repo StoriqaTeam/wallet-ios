a16378654e702aae77003153d1390359fe245102
Daniil Miroshnichecko <>
2018-11-30 14:33:59 +0300

    Fix timoumt deposit polling

--- Wallet/Classes/BusinessLayer/Application/Application.swift
+++ Wallet/Classes/BusinessLayer/Application/Application.swift
@@ -205,7 +205,7 @@ class Application {
     
     // MARK: - Global services -
     lazy var shortPollingTimer: ShortPollingTimerProtocol = ShortPollingTimer(timeout: 60)
-    lazy var depositShortPollintTimer: DepositShortPollingTimerProtocol = DepositShortPollingTimer(timeout: 5)
+    lazy var depositShortPollintTimer: DepositShortPollingTimerProtocol = DepositShortPollingTimer(timeout: 10)
     
     // MARK: - Channels -
     lazy var channelStorage: ChannelStorage = ChannelStorage()
