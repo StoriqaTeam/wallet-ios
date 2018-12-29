e64848be130b88ea37004167762d33644e90fa7d
Tata <t.grigorevskaya@storiqa.com>
2018-12-29 12:33:29 +0300

    localizations + renaming

---
+++ Wallet/Classes/PresentationLayer/ExchangeScenes/RecipientAccounts/Strings+RecipientAccounts.swift
@@ -0,0 +1,18 @@
+//
+//  Strings+RecipientAccounts.swift
+//  Wallet
+//
+//  Created by Storiqa on 29/12/2018.
+//  Copyright © 2018 Storiqa. All rights reserved.
+//
+
+import Foundation
+
+extension Strings {
+    enum RecipientAccounts {
+        static let navigationTitle = NSLocalizedString("RecipientAccounts.navigationTitle",
+                                                       tableName: "RecipientAccounts",
+                                                       value: "Choose account",
+                                                       comment: "Screen title")
+    }
+}
